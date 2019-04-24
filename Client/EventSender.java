8361d4b8dc3b7308a78920a701ced2a9d919b411
Md. Maruf Al Alif Khan <maruf.alif03@gmail.com>
2019-04-24 16:32:16 +0600

    EventSender updated

--- Client/EventSender.java
+++ Client/EventSender.java
@@ -1,3 +1,16 @@
+import java.lang.Runnable;
+import java.net.Socket;
+import java.io.*;
+
 public class EventSender {
-    
+    private Socket socket = null;
+    public EventSender(Socket socket){
+        this.socket = socket;
+    }
+    public void sendMouseEvent(int x, int y){
+
+    }
+    public void sendKeypressEvent(){
+        
+    }
 }
